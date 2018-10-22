b1eaca0d5f8efe140ef72cfd5dafa259f93df225
alvinlai808 <35281838+alvinlai808@users.noreply.github.com>
2018-10-22 11:31:03 -0700

    Update Deque.java

--- Homework/Homework02/Deque.java
+++ Homework/Homework02/Deque.java
@@ -55,9 +55,9 @@ public class Deque {
        return -1;
      } else {
       left++;
- 			if(left >= maxSize)
+         if(left >= maxSize)
         left = 0;
-   			nItems--;
+            nItems--;
         return front;
      }
    }
@@ -104,56 +104,4 @@ public class Deque {
      } System.out.println();
    }
 //--------------------------------------------------------------
-   public static void main(String args[]) {
-     Deque testDeque = new Deque(10);
-
-     System.out.println("\n---Testing insertLeft() and removeLeft()---\n");
-     System.out.println("Inserting in left...\n");
-
-     testDeque.insertLeft(6);
-     testDeque.insertLeft(18);
-     testDeque.insertLeft(27);
-     testDeque.display();
-
-     System.out.println("Removing from left...\n");
-
-     testDeque.removeLeft();
-     testDeque.display();
-     testDeque.removeLeft();
-     testDeque.display();
-
-     testDeque.insertLeft(41);
-     testDeque.insertLeft(2);
-     testDeque.insertLeft(37);
-     testDeque.insertLeft(21);
-     testDeque.insertLeft(196);
-     System.out.println("Left- \n");
-     testDeque.display();
-     System.out.println("-Right");
-
-     System.out.println("\n---Testing insertRight() and removeRight()---\n");
-     System.out.println("Inserting in right...\n");
-
-     testDeque.insertRight(32);
-     testDeque.insertRight(8);
-     testDeque.insertRight(93);
-     System.out.println("Left- \n");
-     testDeque.display();
-     System.out.println("-Right");
-
-     System.out.println("\nRemoving from right...\n");
-
-     testDeque.removeRight();
-     testDeque.removeRight();
-     testDeque.display();
-
-     testDeque.insertRight(234);
-     testDeque.insertRight(3);
-     testDeque.insertRight(29);
-     testDeque.insertRight(12);
-     testDeque.insertRight(31);
-     System.out.println("Left- \n");
-     testDeque.display();
-     System.out.println("-Right");
-   }
 }
